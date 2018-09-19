7f03a5577b860b225b254ea154cd11ba160a6d4a
Steve Anderson <steve@fuzzwork.co.uk>
2018-09-19 21:45:25 +0200

    update

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $memcache = new Memcache;
 $memcache->connect('localhost', 11211) or die ("Could not connect");
 
 require_once('db.inc.php');
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 ?>
 <html>
 <head>
@@ -21,195 +22,94 @@ require_once('db.inc.php');
 </head>
 <body>
 <?php include('/home/web/fuzzwork/htdocs/menu/menubootstrap.php'); ?>
+<?php
+if (isset($_GET['skill']) and is_numeric($_GET['skill'])) {
+    $skill=$_GET['skill'];
+    $refine=(50+$skill)/100;
+} else {
+    $refine=0.5;
+}
+?>
 <div class="container">
 
 <p>Information on Reactions, with some pricing. Alchemy reactions will have the full output priced out</p>
-
+<div class="alert warning">Due to refining changes, all Unrefined materials are being calculated at a 50% return. You need scrap metal processing to increase this, by 1% per level. I /think/ I've got it working right. If you can tell me one way or another, please do.<form action="https://www.fuzzwork.co.uk/reactions/" method="get"><select name="skill"><option>1<option>2<option>3<option>4<option>5</select><input type=submit></form></div>
 <table>
-<?
+<?php
 
-$sql='select typeid,typeName,marketGroupID from invTypes where marketGroupID in (508,509,1143) order by marketgroupid,typeid asc';
+$sql='select invTypes.typeid,typeName,marketGroupID,time from invTypes join industryActivity on invTypes.typeid=industryActivity.typeid where activityid=11 and published=1 order by marketgroupid';
 
 $stmt = $dbh->prepare($sql);
-$sql2="select invTypes.typeid typeid,typename,quantity,input,output,volume from invTypeReactions,invTypes,evesupport.reactionmodifier where invTypeReactions.reactiontypeid=? and invTypeReactions.typeid=invTypes.typeid and evesupport.reactionmodifier.reactiontypeid=invTypeReactions.reactiontypeid order by input asc";
-$reactiondata= $dbh->prepare($sql2);
-$sql3="select typename,invTypeMaterials.materialTypeID typeid,quantity,volume from invTypeMaterials,invTypes where invTypeMaterials.typeid=? and invTypes.typeid=invTypeMaterials.materialTypeID";
-$refinedata= $dbh->prepare($sql3);
-
-$polymersql="select invTypes.typeid typeid,typename,quantity,input,volume from invTypeReactions,invTypes where invTypeReactions.reactiontypeid=? and invTypeReactions.typeid=invTypes.typeid order by input asc";
-$polymerstmt=$dbh->prepare($polymersql);
-
 
 $stmt->execute();
 
-while ($row = $stmt->fetchObject()){
-    $inputs=array();
-    $output=array();
+$reactiondata='select materialtypeid,quantity,typename,volume from industryActivityMaterials iam join invTypes it on iam.materialtypeid=it.typeid  where iam.typeid=:typeid and activityid=11';
+$reactionstmt=$dbh->prepare($reactiondata);
+$productdata='select producttypeid,quantity,typename,volume from industryActivityProducts iap join invTypes it on iap.producttypeid=it.typeid  where iap.typeid=:typeid and activityid=11';
+$productstmt=$dbh->prepare($productdata);
+
+while ($row = $stmt->fetchObject()) {
     $cost=0;
     $price=0;
     $volumein=0;
     $volumeout=0;
     $name=$row->typeName;
-    if (strpos($name,"Unrefined") === false)
-    {
-        # not alchemy
-        echo "<tr><th colspan=4 class='group-".$row->marketGroupID."'>$name</th></tr>";
-        if ($row->marketGroupID==1143)
-        {
-            $polymerstmt->execute(array($row->typeid));
-            while ($reactions = $polymerstmt->fetchObject()){
-                $arraykey=$reactions->typeid.'|'.$reactions->typename.'|'.$reactions->volume;
-                if ($reactions->input){
-                    $inputs[$arraykey]=$reactions->quantity;
-                }
-                else {
-                    $output[$arraykey]=$reactions->quantity;
-                }
-            }
-
-        } else {
-            $reactiondata->execute(array($row->typeid));
-            while ($reactions = $reactiondata->fetchObject()){
-                $arraykey=$reactions->typeid.'|'.$reactions->typename.'|'.$reactions->volume;
-                if ($reactions->input){
-                    $inputs[$arraykey]=$reactions->quantity*100;
-                }
-                else {
-                    $output[$arraykey]=$reactions->output;
-                }
-            }
-        }
-        echo "<tr><td><table border=1><thead><tr><th colspan=4>Input</th><tr><th>Material</th><th>Quantity</th><th>Volume m3</th><th>Total cost</th></tr></thead>\n";
-
-        foreach ($inputs as $input=>$quantity){
-            echo "<tr><td>";
-            $matdata=explode("|",$input);
-            echo $matdata[1]."</td><td class='number'>".($quantity)."</td><td class='number'>".($quantity*$matdata[2])."</td><td class='number'>";
-            $pricedatasell=$memcache->get('forgesell-'.$matdata[0]);
-            $values=explode("|",$pricedatasell);
-            $innerprice=$values[0];
-            if ($innerprice=="")
-            {
+    echo "<tr><th colspan=4 class='group-".$row->marketGroupID."'>$name</th></tr>";
+    $reactionstmt->execute(array(":typeid"=>$row->typeid));
+    $productstmt->execute(array(":typeid"=>$row->typeid));
+    echo "<tr><td><table border=1><thead><tr><th colspan=4>Input</th><tr>";
+    echo "<th>Material</th><th>Quantity</th><th>Volume m3</th><th>Total cost</th></tr></thead>\n";
+    while ($reactions = $reactionstmt->fetchObject()) {
+        echo "<tr><td>";
+        echo $reactions->typename."</td><td class='number'>".$reactions->quantity;
+        echo "</td><td class='number'>".($reactions->quantity*$reactions->volume)."</td><td class='number'>";
+        $pricedatasell=$memcache->get('forgesell-'.$reactions->materialtypeid);
+        $values=explode("|", $pricedatasell);
+        $innerprice=$values[0];
+        if ($innerprice=="") {
                 $innerprice=0;
-            }
-            $value=$innerprice*$quantity;
-            $cost+=$value;
-            $volumein+=$quantity*$matdata[2];
-            echo number_format($value)."</td></tr>\n";
         }
-        echo "<tr><td colspan=2>Overall</td><td class='number'>$volumein</td><td class='number'>".number_format($cost)."</td></tr></table></td>\n";
-        echo "<td><table border=1><thead><tr><th colspan=4>Output</th></tr><tr><th>Material</th><th>Quantity</th><th>Volume m3</th><th>Total value</th></tr></thead>\n";
-        foreach ($output as $output=>$quantity){
-            echo "<tr><td>";
-            $matdata=explode("|",$output);
-            echo $matdata[1]."</td><td class='number'>".($quantity)."</td><td class='number'>".($quantity*$matdata[2])."</td><td class='number'>";
-            $pricedatasell=$memcache->get('forgesell-'.$matdata[0]);
-            $values=explode("|",$pricedatasell);
-            $innerprice=$values[0];
-            if ($innerprice=="")
-            {
-                $innerprice=0;
-            }
-            $value=$innerprice*$quantity;
-            $price+=$value;
-            $volumeout+=$quantity*$matdata[2];
-            echo number_format($value)."</td></tr>\n";
-        }
-        echo "<tr><td colspan=2>Overall</td><td class='number'>$volumeout</td><td class='number'>".number_format($price)."</td></tr></table></td>\n";
-
-echo "<td><table border=1><thead><tr><th colspan=4>Profits</th></tr><tr><th>Period</th><th>Margin</th><th>Volume in</th><th>Volume out</th></tr></thead><tr><td>One run</td><td class='number'>".number_format($price-$cost)."</td><td class='number'>".number_format($volumein)."</td><td class='number'>".number_format($volumeout)."</td></tr><tr><td>One day</td><td class='number'>".number_format(($price-$cost)*24)."</td><td class='number'>".number_format($volumein*24)."</td><td class='number'>".number_format($volumeout*24)."</td></tr><tr><td>Thirty days</td><td class='number'>".number_format(($price-$cost)*24*30)."</td><td class='number'>".number_format($volumein*24*30)."</td><td class='number'>".number_format($volumeout*24*30)."</td></tr></table><td class='number'>".number_format(($price-$cost)*24*30)."</td></tr>";
-
-
-
-        echo "</tr>\n";
-
+        $value=$innerprice*$reactions->quantity;
+        $cost+=$value;
+        $volumein+=$reactions->quantity*$reactions->volume;
+        echo number_format($value)."</td></tr>\n";
     }
-    else
-    {
-    # Alchemy! So we have to refine as well.
-        echo "<tr><th colspan=4 class='group-".$row->marketGroupID."'>$name</th></tr>";
-        $reactiondata->execute(array($row->typeid));
-        while ($reactions = $reactiondata->fetchObject()){
-            $arraykey=$reactions->typeid.'|'.$reactions->typename.'|'.$reactions->volume;
-            if ($reactions->input){
-                $inputs[$arraykey]=$reactions->quantity*100;
-            }
-            else {
-                $output[$arraykey]=$reactions->output;
-            }
+    echo "<tr><td colspan=2>Overall</td><td class='number'>$volumein</td><td class='number'>".number_format($cost)."</td></tr></table></td>\n";
+    echo "<td><table border=1><thead><tr><th colspan=4>Output</th></tr><tr><th>Material</th><th>Quantity</th><th>Volume m3</th><th>Total value</th></tr></thead>\n";
+    while ($products = $productstmt->fetchObject()) {
+        echo "<tr><td>";
+        echo $products->typename."</td><td class='number'>".$products->quantity."</td><td class='number'>".($products->quantity*$products->volume)."</td><td class='number'>";
+        $pricedatasell=$memcache->get('forgesell-'.$products->producttypeid);
+        $values=explode("|", $pricedatasell);
+        $innerprice=$values[0];
+        if ($innerprice=="") {
+            $innerprice=0;
         }
-        echo "<tr><td><table border=1><thead><tr><th colspan=4>Input</th><tr><th>Material</th><th>Quantity</th><th>Volume m3</th><th>Total cost</th></tr></thead>\n";
-
-        foreach ($inputs as $input=>$quantity){
-            echo "<tr><td>";
-            $matdata=explode("|",$input);
-            echo $matdata[1]."</td><td class='number'>".($quantity)."</td><td class='number'>".($quantity*$matdata[2])."</td><td class='number'>";
-            $pricedatasell=$memcache->get('forgesell-'.$matdata[0]);
-            $values=explode("|",$pricedatasell);
-            $innerprice=$values[0];
-            if ($innerprice=="")
-            {
-                $innerprice=0;
-            }
-            $value=$innerprice*$quantity;
-            $cost+=$value;
-            $volumein+=$quantity*$matdata[2];
-            echo number_format($value)."</td></tr>\n";
-        }
-        echo "<tr><td colspan=2>Overall</td><td class='number'>$volumein</td><td class='number'>".number_format($cost)."</td></tr></table></td>\n";
-        echo "<td><table border=1><thead><tr><th colspan=3>Output, unrefined</th></tr><tr><th>Material</th><th>Quantity</th><th>Total value</th></tr></thead>\n";
-        $unrefined=array();
-        foreach ($output as $output=>$quantity){
-            echo "<tr><td>";
-            $matdata=explode("|",$output);
-            echo $matdata[1]."</td><td class='number'>".($quantity)."</td><td class='number'>";
-            $pricedatasell=$memcache->get('forgesell-'.$matdata[0]);
-            $values=explode("|",$pricedatasell);
-            $innerprice=$values[0];
-            if ($innerprice=="")
-            {
-                $innerprice=0;
-            }
-            $value=$innerprice*$quantity;
-            $price+=$value;
-            echo number_format($value)."</td></tr>\n";
-            $unrefined[$matdata[0]]=$quantity;
-        }
-        $price=0;
-        echo "</table><table border=1><thead><tr><th colspan=4>Output, refined</th></tr><tr><th>Material</th><th>Quantity</th><th>Volume m3</th><th>Total value</th></tr></thead>\n";
-        foreach ($unrefined as $material=>$quantity){
-            $refinedata->execute(array($material)); 
-            while ($refining = $refinedata->fetchObject()){
-                echo "<tr><td>";
-                echo $refining->typename."</td><td class='number'>".($refining->quantity*$quantity)."</td><td class='number'>".($refining->quantity*$quantity*$refining->volume)."</td><td class='number'>";
-                $pricedatasell=$memcache->get('forgesell-'.$refining->typeid);
-                $values=explode("|",$pricedatasell);
-                $innerprice=$values[0];
-                if ($innerprice=="")
-                {
-                    $innerprice=0;
-                }
-                $value=$innerprice*$quantity*$refining->quantity;
-                $volumeout+=$quantity*$refining->quantity*$refining->volume;
-                $price+=$value;
-                echo number_format($value)."</td></tr>\n";
-            }
-        } 
-        echo "<tr><td colspan=2>Overall</td><td class='number'>$volumeout</td><td class='number'>".number_format($price)."</td></tr></table>\n";
-
-
-
-
-
-
-        echo "<td><table border=1><thead><tr><th colspan=4>Profits</th></tr><tr><th>Period</th><th>Margin</th><th>Volume in</th><th>Volume out</th></tr></thead><tr><td>One run</td><td class='number'>".number_format($price-$cost)."</td><td class='number'>".number_format($volumein)."</td><td class='number'>".number_format($volumeout)."</td></tr><tr><td>One day</td><td class='number'>".number_format(($price-$cost)*24)."</td><td class='number'>".number_format($volumein*24)."</td><td class='number'>".number_format($volumeout*24)."</td></tr><tr><td>Thirty days</td><td class='number'>".number_format(($price-$cost)*24*30)."</td><td class='number'>".number_format($volumein*24*30)."</td><td class='number'>".number_format($volumeout*24*30)."</td></tr></table></td><td class='number'>".number_format(($price-$cost)*24*30)."</td></tr>";
-
+        $value=$innerprice*$products->quantity;
+        $price+=$value;
+        $volumeout+=$products->quantity*$products->volume;
+        echo number_format($value)."</td></tr>\n";
     }
+    echo "<tr><td colspan=2>Overall</td><td class='number'>$volumeout</td><td class='number'>".number_format($price)."</td></tr></table></td>\n";
+
+    echo "<td><table border=1><thead><tr><th colspan=5>Profits</th></tr>";
+    echo "<tr><th>Period</th><th>Margin</th><th>Volume in</th><th>Volume out</th><th>Cycles</th></tr></thead>";
+    echo "<tr><td>One run</td><td class='number'>".number_format($price-$cost)."</td>";
+    echo "<td class='number'>".number_format($volumein)."</td>";
+    echo "<td class='number'>".number_format($volumeout)."</td><td>1</td></tr>";
+    $cycles=floor(86400/$row->time);
+    echo "<tr><td>One day</td><td class='number'>".number_format(($price-$cost)*$cycles)."</td>";
+    echo "<td class='number'>".number_format($volumein*$cycles)."</td><td class='number'>".number_format($volumeout*$cycles)."</td>";
+    echo "<td>$cycles</td></tr>";
+    $cycles=floor((30*86400)/$row->time);
+    echo "<tr><td>Thirty days</td><td class='number'>".number_format(($price-$cost)*$cycles)."</td>";
+    echo "<td class='number'>".number_format($volumein*$cycles)."</td><td class='number'>".number_format($volumeout*$cycles)."</td>";
+    echo "<td>$cycles</td></tr>";
+    echo "</table><td class='number'>".number_format(($price-$cost)*$cycles)."</td></tr>";
 
 
 
-
-
+    echo "</tr>\n";
 }
 ?>
 </table>
